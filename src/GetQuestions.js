4924157e32b69250e625dd32c7850921bf66c310
Prashastha01 <kovuriprashastha01@gmail.com>
2021-06-03 23:12:34 +0530

    saved question to db and displayed

---
+++ src/GetQuestions.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import { CheckCircleSharp } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 200,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+function GetQuestions() {
+  const classes = useStyles();
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    db.collection('questions').onSnapshot((snapshot) => {
+      setQuestions(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ques: doc.data(),
+        }))
+      );
+    });
+    console.log(questions);
+  }, []);
+  return (
+    <div>
+      {questions.map(({ id, ques }, index) => (
+        <Card className={classes.root} variant='outlined'>
+          <CardContent>
+            <Typography variant='h5' component='h2'>
+              {index + 1 + '. ' + ques.question}
+            </Typography>
+            <Typography className={classes.pos} color='textSecondary'>
+              {ques.option1 === ques.answer ? (
+                <CheckCircleSharp style={{ color: 'green' }} />
+              ) : (
+                ''
+              )}
+              {' ' + ques.option1}
+            </Typography>
+            <Typography className={classes.pos} color='textSecondary'>
+              {ques.option2 === ques.answer ? (
+                <CheckCircleSharp style={{ color: 'green' }} />
+              ) : (
+                ''
+              )}
+              {' ' + ques.option2}
+            </Typography>
+            <Typography className={classes.pos} color='textSecondary'>
+              {ques.option3 === ques.answer ? (
+                <CheckCircleSharp style={{ color: 'green' }} />
+              ) : (
+                ''
+              )}
+              {' ' + ques.option3}
+            </Typography>
+            <Typography className={classes.pos} color='textSecondary'>
+              {ques.option4 === ques.answer ? (
+                <CheckCircleSharp style={{ color: 'green' }} />
+              ) : (
+                ''
+              )}
+              {' ' + ques.option4}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default GetQuestions;
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { fire } from './firebase.js';
-import { AddTwoTone } from '@material-ui/icons';
+import { fire, db } from './firebase.js';
+import { AddTwoTone, ExitToAppTwoTone } from '@material-ui/icons';
 import {
   Button,
   Dialog,
@@ -9,20 +9,21 @@ import {
   DialogContentText,
   DialogTitle,
   FormControl,
-  Divider,
   Radio,
   RadioGroup,
   TextField,
   FormControlLabel,
 } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
+import firebase from 'firebase';
+import GetQuestions from './GetQuestions';
 
 const useStyles = makeStyles((theme) => ({
   form: {
     display: 'flex',
     flexDirection: 'column',
     margin: 'auto',
-    width: 'fit-content',
   },
   formControl: {
     marginTop: theme.spacing(2),
@@ -32,7 +33,9 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(1),
   },
 }));
-
+function Alert(props) {
+  return <MuiAlert elevation={6} variant='filled' {...props} />;
+}
 function Home() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
@@ -43,7 +46,8 @@ function Home() {
   const [option2, setOption2] = React.useState('');
   const [option3, setOption3] = React.useState('');
   const [option4, setOption4] = React.useState('');
-  const [chooseAnswer, setChooseAnswer] = React.useState(0);
+  const [chooseAnswer, setChooseAnswer] = React.useState(option1);
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -51,6 +55,25 @@ function Home() {
   const handleClose = () => {
     console.log(question, option1, option2, option3, option4);
     setOpen(false);
+  };
+  const uploadQuestion = () => {
+    handleClose();
+    db.collection('questions').add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      cName: firebase.auth().currentUser.displayName,
+      cId: firebase.auth().currentUser.uid,
+      question: question,
+      option1: option1,
+      option2: option2,
+      option3: option3,
+      option4: option4,
+      answer: chooseAnswer,
+    });
+    setQuestion('');
+    setOption1('');
+    setOption2('');
+    setOption3('');
+    setOption4('');
   };
 
   const handleChange = (event) => {
@@ -69,6 +92,7 @@ function Home() {
     setOption4(event.target.value);
   };
   const handleSetAnswer = (event) => {
+    console.log('here', event.target.value);
     setChooseAnswer(event.target.value);
   };
   return (
@@ -81,6 +105,9 @@ function Home() {
       >
         Add Question
       </Button>
+
+      <br />
+      <GetQuestions />
       <Dialog
         fullWidth={fullWidth}
         maxWidth={maxWidth}
@@ -88,10 +115,10 @@ function Home() {
         onClose={handleClose}
         aria-labelledby='max-width-dialog-title'
       >
-        <DialogTitle id='max-width-dialog-title'>Optional sizes</DialogTitle>
+        <DialogTitle id='max-width-dialog-title'>Add your question</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            You can set my maximum width and whether to adapt or not.
+            {/* {You can set my maximum width and whether to adapt or not.} */}
           </DialogContentText>
           <form className={classes.form} noValidate>
             <FormControl className={classes.formControl}>
@@ -110,6 +137,7 @@ function Home() {
               />
               <RadioGroup value={chooseAnswer} onChange={handleSetAnswer}>
                 <FormControlLabel
+                  value={option1}
                   control={<Radio />}
                   label={
                     <TextField value={option1} onChange={handleChangeOp1} />
@@ -117,18 +145,21 @@ function Home() {
                 />
                 <FormControlLabel
                   control={<Radio />}
+                  value={option2}
                   label={
                     <TextField value={option2} onChange={handleChangeOp2} />
                   }
                 />
                 <FormControlLabel
                   control={<Radio />}
+                  value={option3}
                   label={
                     <TextField value={option3} onChange={handleChangeOp3} />
                   }
                 />
                 <FormControlLabel
                   control={<Radio />}
+                  value={option4}
                   label={
                     <TextField value={option4} onChange={handleChangeOp4} />
                   }
@@ -138,15 +169,28 @@ function Home() {
           </form>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color='primary'>
-            Close
+          <Button onClick={uploadQuestion} color='primary'>
+            Save Question
           </Button>
+          {/* <Snackbar
+            open={openSnacbar}
+            autoHideDuration={6000}
+            onClose={closeSnackbar}
+          >
+            <Alert onClose={closeSnackbar} severity='success'>
+              This is a success message!
+            </Alert>
+          </Snackbar> */}
         </DialogActions>
       </Dialog>
+
       <Button
+        startIcon={<ExitToAppTwoTone />}
         onClick={() => {
           fire.auth().signOut();
         }}
+        color='primary'
+        variant='outlined'
       >
         Logout
       </Button>
